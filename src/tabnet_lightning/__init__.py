8d14266559830ab5ffcad511f3336b93f7c65fda
clemens33 <yongwu33@gmail.com>
2021-04-03 20:48:59 +0200

    - added lightning wrapper skeleton

---
+++ src/tabnet_lightning/__init__.py
@@ -0,0 +1,8 @@
+from .classifier import TabNetClassifier
+from .regressor import TabNetRegressor
+
+__author__ = "Clemens Kriechbaumer"
+__email__ = "clemens.kr@gmail.com"
+
+__info__ = "pytorch lightning wrapper for tabnet - work in progress"
+
---
+++ src/tabnet_lightning/regressor.py
@@ -0,0 +1,9 @@
+import pytorch_lightning as pl
+
+from tabnet import TabNet
+
+
+class TabNetRegressor(pl.LightningModule):
+    # TODO
+
+    pass
