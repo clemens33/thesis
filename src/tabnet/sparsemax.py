c93f9a0b53d35e1eb51d96550b02d2ed7ac6102a
clemens33 <yongwu33@gmail.com>
2021-04-05 21:24:56 +0200

    - added alternative sparsemax

--- src/tabnet/sparsemax.py
+++ src/tabnet/sparsemax.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 
-class _Sparsemax(torch.autograd.Function):
+class _Sparsemax1(torch.autograd.Function):
     """adapted from https://github.com/aced125/sparsemax/tree/master/sparsemax"""
 
     @staticmethod
@@ -20,7 +20,7 @@ class _Sparsemax(torch.autograd.Function):
         ctx.dim = dim
 
         if ctx.needs_reshaping:
-            ctx, input = _Sparsemax._flatten_all_but_nth_dim(ctx, input)
+            ctx, input = _Sparsemax1._flatten_all_but_nth_dim(ctx, input)
 
         # Translate by max for numerical stability
         input = input - input.max(-1, keepdim=True).values.expand_as(input)
@@ -48,7 +48,7 @@ class _Sparsemax(torch.autograd.Function):
 
         # Reshape back to original shape
         if ctx.needs_reshaping:
-            ctx, output = _Sparsemax._unflatten_all_but_nth_dim(ctx, output)
+            ctx, output = _Sparsemax1._unflatten_all_but_nth_dim(ctx, output)
 
         return output
 
@@ -58,7 +58,7 @@ class _Sparsemax(torch.autograd.Function):
 
         # Reshape if needed
         if ctx.needs_reshaping:
-            ctx, grad_output = _Sparsemax._flatten_all_but_nth_dim(ctx, grad_output)
+            ctx, grad_output = _Sparsemax1._flatten_all_but_nth_dim(ctx, grad_output)
 
         # Compute gradient
         nonzeros = torch.ne(output, 0)
@@ -68,7 +68,7 @@ class _Sparsemax(torch.autograd.Function):
 
         # Reshape back to original shape
         if ctx.needs_reshaping:
-            ctx, grad_input = _Sparsemax._unflatten_all_but_nth_dim(ctx, grad_input)
+            ctx, grad_input = _Sparsemax1._unflatten_all_but_nth_dim(ctx, grad_input)
 
         return grad_input, None
 
@@ -107,12 +107,97 @@ class _Sparsemax(torch.autograd.Function):
         return ctx, x.transpose(0, ctx.dim)
 
 
+class _Sparsemax2(torch.autograd.Function):
+    # credits to Yandex https://github.com/Qwicen/node/blob/master/lib/nn_utils.py
+    # TODO this version fails gradient checking - refer to tests - check why?
+    """
+    An implementation of sparsemax (Martins & Astudillo, 2016). See
+    :cite:`DBLP:journals/corr/MartinsA16` for detailed description.
+    By Ben Peters and Vlad Niculae
+    """
+
+    @staticmethod
+    def forward(ctx, input, dim=-1):  # noqa
+        """sparsemax: normalizing sparse transform (a la softmax)
+
+        Parameters
+        ----------
+        ctx : torch.autograd.function._ContextMethodMixin
+        input : torch.Tensor
+            any shape
+        dim : int
+            dimension along which to apply sparsemax
+
+        Returns
+        -------
+        output : torch.Tensor
+            same shape as input
+
+        """
+        ctx.dim = dim
+        max_val, _ = input.max(dim=dim, keepdim=True)
+        input -= max_val  # same numerical stability trick as for softmax
+        tau, supp_size = _Sparsemax2._threshold_and_support(input, dim=dim)
+        output = torch.clamp(input - tau, min=0)
+        ctx.save_for_backward(supp_size, output)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):  # noqa
+        supp_size, output = ctx.saved_tensors
+        dim = ctx.dim
+        grad_input = grad_output.clone()
+        grad_input[output == 0] = 0
+
+        v_hat = grad_input.sum(dim=dim) / supp_size.to(output.dtype).squeeze()
+        v_hat = v_hat.unsqueeze(dim)
+        grad_input = torch.where(output != 0, grad_input - v_hat, grad_input)
+        return grad_input, None
+
+    @staticmethod
+    def _threshold_and_support(input, dim=-1):
+        """Sparsemax building block: compute the threshold
+
+        Parameters
+        ----------
+        input: torch.Tensor
+            any dimension
+        dim : int
+            dimension along which to apply the sparsemax
+
+        Returns
+        -------
+        tau : torch.Tensor
+            the threshold value
+        support_size : torch.Tensor
+
+        """
+
+        input_srt, _ = torch.sort(input, descending=True, dim=dim)
+        input_cumsum = input_srt.cumsum(dim) - 1
+        rhos = _Sparsemax2._make_ix_like(input, dim)
+        support = rhos * input_srt > input_cumsum
+
+        support_size = support.sum(dim=dim).unsqueeze(dim)
+        tau = input_cumsum.gather(dim, support_size - 1)
+        tau /= support_size.to(input.dtype)
+        return tau, support_size
+
+    @staticmethod
+    def _make_ix_like(input, dim=0):
+        d = input.size(dim)
+        rho = torch.arange(1, d + 1, device=input.device, dtype=input.dtype)
+        view = [1] * input.dim()
+        view[0] = -1
+        return rho.view(view).transpose(0, dim)
+
+
 class Sparsemax(nn.Module):
     def __init__(self, dim: int = -1):
         super(Sparsemax, self).__init__()
 
         self.dim = dim
-        self.sparsemax = _Sparsemax.apply
+        self.sparsemax = _Sparsemax1.apply
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         return self.sparsemax(input, self.dim)
