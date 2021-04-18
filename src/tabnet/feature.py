- fixed bug for shared layers - restructured layers / batch normalization is not shared anymore! - adapted test cases

src/tabnet/feature.py
@@ -17,11 +17,31 @@ class GLU(nn.Module):
         return input[..., :self.n_units] * torch.sigmoid(input[..., self.n_units:])
 
 
-class FeatureLayer(nn.Module):
+class Layer(nn.Module):
     def __init__(self, input_size: int, feature_size: int, bias: bool = False, **kwargs):
+        super(Layer, self).__init__()
+
+        self.input_size = input_size
+        self.feature_size = feature_size
+
+        self.fc = nn.Linear(in_features=input_size, out_features=feature_size, bias=bias)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return self.fc(input)
+
+
+class FeatureLayer(nn.Module):
+    """feature layer using gated linear unit activation"""
+
+    def __init__(self, input_size: int, feature_size: int, shared_layer: Optional[Layer] = None, **kwargs):
         super(FeatureLayer, self).__init__()
 
-        self.fc = nn.Linear(in_features=input_size, out_features=feature_size * 2, bias=bias)
+        if shared_layer is not None:
+            assert shared_layer.input_size == input_size, f"shared_layer in_features {shared_layer.input_size} do not match input_size {input_size}"
+            assert shared_layer.feature_size == feature_size * 2, f"shared_layer out_features {shared_layer.feature_size} do not match feature_size * 2 {feature_size}"
+
+        self.fc = shared_layer if shared_layer is not None else FeatureLayer.init_layer(input_size=input_size, feature_size=feature_size,
+                                                                                        **kwargs)
         self.bn = GhostBatchNorm1d(input_size=feature_size * 2, **kwargs)
 
         self.glu = GLU(n_units=feature_size)
@@ -34,9 +54,14 @@ class FeatureLayer(nn.Module):
 
         return feature
 
+    @staticmethod
+    def init_layer(input_size: int, feature_size: int, **kwargs) -> Layer:
+        """custom init function accounting for the double feature size due to the gated linear unit activation"""
+        return Layer(input_size=input_size, feature_size=feature_size * 2, **kwargs)
+
 
 class FeatureTransformer(nn.Module):
-    def __init__(self, input_size: int, feature_size: int, nr_layers: int = 1, shared_layers: Optional[List[FeatureLayer]] = None,
+    def __init__(self, input_size: int, feature_size: int, nr_layers: int = 1, shared_layers: Optional[List[Layer]] = None,
                  normalize: float = math.sqrt(.5), **kwargs):
         super(FeatureTransformer, self).__init__()
 
@@ -46,7 +71,9 @@ class FeatureTransformer(nn.Module):
 
         if shared_layers:
             self.layers = nn.ModuleList(
-                shared_layers +
+                [FeatureLayer(input_size=input_size, feature_size=feature_size, shared_layer=shared_layers[0], **kwargs)] +
+                [FeatureLayer(input_size=feature_size, feature_size=feature_size, shared_layer=shared_layer, **kwargs) for shared_layer in
+                 shared_layers[1:]] +
                 [FeatureLayer(input_size=feature_size, feature_size=feature_size, **kwargs) for _ in range(nr_layers)]
             )
         else:

src/tabnet/model.py
@@ -30,9 +30,9 @@ class TabNet(nn.Module):
         shared_layers = []
         if nr_shared_layers > 0:
             shared_layers.append(
-                FeatureLayer(input_size=input_size, feature_size=feature_size, momentum=momentum, **kwargs))
+                FeatureLayer.init_layer(input_size=input_size, feature_size=feature_size, **kwargs))
             shared_layers += [
-                FeatureLayer(input_size=feature_size, feature_size=feature_size, momentum=momentum, **kwargs)
+                FeatureLayer.init_layer(input_size=feature_size, feature_size=feature_size, **kwargs)
                 for _ in range(1, nr_shared_layers)]
 
         # use ghost batch norm with large virtual batch size - this applies the custom default batch normalization supporting 3D inputs
@@ -42,7 +42,6 @@ class TabNet(nn.Module):
 
         self.steps = nn.ModuleList([Step(input_size=input_size, feature_size=feature_size, decision_size=decision_size, nr_layers=nr_layers,
                                          shared_layers=shared_layers, gamma=gamma, momentum=momentum, **kwargs) for _ in range(nr_steps)])
-
 
     def forward(self, input: torch.Tensor, prior: Optional[torch.Tensor] = None) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         input = self.bn(input)

src/tabnet/step.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 from tabnet.attentive import AttentiveTransformer
-from tabnet.feature import FeatureTransformer, FeatureLayer
+from tabnet.feature import FeatureTransformer, Layer
 
 
 class Step(nn.Module):
@@ -13,7 +13,7 @@ class Step(nn.Module):
                  feature_size: int,
                  decision_size: int,
                  nr_layers: int = 1,
-                 shared_layers: Optional[List[FeatureLayer]] = None,
+                 shared_layers: Optional[List[Layer]] = None,
                  gamma: float = 1.0,
                  decision_activation: nn.Module = nn.ReLU(),
                  **kwargs):

src/tabnet/utils.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class GhostBatchNorm1d(nn.Module):
-    def __init__(self, input_size: int, momentum: float = 0.1, virtual_batch_size: int = 8):
+    def __init__(self, input_size: int, momentum: float = 0.1, virtual_batch_size: int = 8, **kwargs):
         super(GhostBatchNorm1d, self).__init__()
 
         self.input_size = input_size
@@ -30,11 +30,11 @@ class GhostBatchNorm1d(nn.Module):
         input = input.transpose(-1, 1) if input.ndim == 3 else input
 
         # vb is set to 1 in the original tabnet tf implementation during evaluation - TODO check why?
-        # vb = self.virtual_batch_size if self.training else 1
-        vb = self.virtual_batch_size
+        vbs = self.virtual_batch_size
+        # vbs = self.virtual_batch_size if self.training else 1
 
         # apply batch norm per chunk
-        chunks = torch.split(input, vb, dim=0)
+        chunks = torch.split(input, vbs, dim=0)
         output = [self.bn(chunk) for chunk in chunks]
         output = torch.cat(output, dim=0)
 