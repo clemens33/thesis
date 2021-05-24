06b8d2995a886a30b8b48ce3e2dad86be098b5e5
clemens33 <yongwu33@gmail.com>
2021-05-24 11:11:37 +0200

    - added ecfc featurizer

--- src/datasets/featurizer.py
+++ src/datasets/featurizer.py
@@ -5,6 +5,58 @@ from sklearn.feature_extraction import DictVectorizer
 
 import numpy as np
 
+from argparse import Namespace
+from pathlib import Path, PurePosixPath
+from typing import Optional, List, Tuple, Dict
+
+import deepchem
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.loggers import LightningLoggerBase
+from pytorch_lightning.utilities import rank_zero_only
+from rdkit.Chem import AllChem
+from scipy.sparse import csr_matrix, save_npz, load_npz
+from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+
+from datasets.utils import add_noise_features, add_noise
+
+dataset_loading_functions = {
+    "bace_c": deepchem.molnet.load_bace_classification,
+    "bbbp": deepchem.molnet.load_bbbp,
+    "chembl": deepchem.molnet.load_chembl,
+    "clearance": deepchem.molnet.load_clearance,
+    "clintox": deepchem.molnet.load_clintox,
+    "delaney": deepchem.molnet.load_delaney,
+    "factors": deepchem.molnet.load_factors,
+    "hiv": deepchem.molnet.load_hiv,
+    "hopv": deepchem.molnet.load_hopv,
+    "hppb": deepchem.molnet.load_hppb,
+    "kaggle": deepchem.molnet.load_kaggle,
+    "kinase": deepchem.molnet.load_kinase,
+    "lipo": deepchem.molnet.load_lipo,
+    "muv": deepchem.molnet.load_muv,
+    "nci": deepchem.molnet.load_nci,
+    "pcba": deepchem.molnet.load_pcba,
+    # "pcba_128": deepchem.molnet.load_pcba_128,
+    # "pcba_146": deepchem.molnet.load_pcba_146,
+    # "pcba_2475": deepchem.molnet.load_pcba_2475,
+    # "pdbbind": deepchem.molnet.load_pdbbind_grid,
+    "ppb": deepchem.molnet.load_ppb,
+    # "qm7": deepchem.molnet.load_qm7_from_mat,
+    # "qm7b": deepchem.molnet.load_qm7b_from_mat,
+    "qm8": deepchem.molnet.load_qm8,
+    "qm9": deepchem.molnet.load_qm9,
+    "sampl": deepchem.molnet.load_sampl,
+    "sider": deepchem.molnet.load_sider,
+    "thermosol": deepchem.molnet.load_thermosol,
+    "tox21": deepchem.molnet.load_tox21,
+    "toxcast": deepchem.molnet.load_toxcast,
+    "uv": deepchem.molnet.load_uv
+}
+
 
 class ECFC_featurizer():
     def __init__(self, radius=6, min_fragm_occur=50, useChirality=True, useFeatures=False):
@@ -39,3 +91,27 @@ class ECFC_featurizer():
         fp_list = self.compute_fp_list(x_test)
         X_raw = self.v.transform(fp_list)
         return X_raw[:, self.idx_col].toarray()
+
+
+if __name__ == "__main__":
+    name = "bbbp"
+    use_cache = True
+    seed = 1
+
+    tasks, all_dataset, transformers = dataset_loading_functions[name](featurizer="Raw", splitter=None, reload=use_cache)
+    featurizer = ECFC_featurizer()
+
+    X = all_dataset[0].ids
+
+    X_train, X_test = train_test_split(X, test_size=.2, random_state=seed)
+
+    X_train_f0 = featurizer.fit(X_train)
+    X_test_f0 = featurizer.transform(X_test)
+
+    X_f = featurizer.transform(X)
+    X_train_f, X_test_f = train_test_split(X_f, test_size=.2, random_state=seed)
+
+    assert np.array_equal(X_train_f0, X_train_f)
+    assert np.array_equal(X_test_f0, X_test_f)
+
+    print("abc")
--- src/datasets/molnet.py
+++ src/datasets/molnet.py
@@ -14,6 +14,7 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
 from tqdm import tqdm
 
+from datasets.featurizer import ECFC_featurizer
 from datasets.utils import add_noise_features, add_noise
 
 dataset_loading_functions = {
@@ -60,13 +61,14 @@ class MolNetClassifierDataModule(pl.LightningDataModule):
     def __init__(self, name: str,
                  batch_size: int,
                  num_workers: int = 4,
-                 split: str = "random",
+                 split_type: str = "random",
                  split_size: Tuple[float, float, float] = (0.8, 0.1, 0.1),
                  split_seed: int = 5180,
                  cache_dir=str(Path.home()) + "/.cache/molnet/",
                  use_cache: bool = True,
                  noise_features: Optional[Dict] = None,
                  noise: Optional[str] = None,
+                 featurizer_name: str = "ecfp",
                  **kwargs):
         """
 
@@ -74,7 +76,7 @@ class MolNetClassifierDataModule(pl.LightningDataModule):
             name: molnet dataset name
             batch_size: training batch size
             num_workers: num workers used for data loaders
-            split: type of split
+            split_type: type of split
             split_size: split size
             split_seed: seed used for splitting + noise (if defined)
             cache_dir: cache directory
@@ -89,7 +91,7 @@ class MolNetClassifierDataModule(pl.LightningDataModule):
         assert split_size[0] > 0.0, f"train split size must sum greater than 0.0"
 
         self.name = name
-        self.split = split
+        self.split_type = split_type
         self.split_size = split_size
         self.split_seed = split_seed
 
@@ -102,37 +104,67 @@ class MolNetClassifierDataModule(pl.LightningDataModule):
         self.noise_features = noise_features
         self.noise = noise
 
+        self.featurizer_name = featurizer_name
+
         self.kwargs = kwargs
 
     def prepare_data(self):
         """create and cache descriptors using morgan fingerprint vectors if not already done/existing"""
-        n_bits = self.kwargs["n_bits"]
-        radius = self.kwargs["radius"]
-        chirality = self.kwargs["chirality"] if "chirality" in self.kwargs else False
-        features = self.kwargs["features"] if "features" in self.kwargs else False
 
-        tasks, all_dataset, transformers = dataset_loading_functions[self.name](featurizer="Raw", splitter=None, data_dir=self.cache_dir,
-                                                                                reload=self.use_cache)
+        if self.featurizer_name == "ecfp":
+            n_bits = self.kwargs["n_bits"]
+            radius = self.kwargs["radius"]
+            chirality = self.kwargs["chirality"] if "chirality" in self.kwargs else False
+            features = self.kwargs["features"] if "features" in self.kwargs else False
 
-        cached_descriptors = self.cache_dir + self.name + "_ecfp" + f"_radius{str(radius)}" + f"_n_bits{str(n_bits)}" + f"_chirality{str(chirality)}" + f"_features{str(features)}" + ".npz"
+            tasks, all_dataset, transformers = dataset_loading_functions[self.name](featurizer="Raw", splitter=None,
+                                                                                    data_dir=self.cache_dir,
+                                                                                    reload=self.use_cache)
 
-        if Path(PurePosixPath(cached_descriptors)).exists() and self.use_cache:
-            return
+            cached_descriptors = self.cache_dir + self.name + "_ecfp" + f"_radius{str(radius)}" + f"_n_bits{str(n_bits)}" + f"_chirality{str(chirality)}" + f"_features{str(features)}" + ".npz"
 
-        desc_mat = np.zeros((len(all_dataset[0].X), n_bits))
+            if Path(PurePosixPath(cached_descriptors)).exists() and self.use_cache:
+                return
 
-        pbar = tqdm(all_dataset[0].X)
-        for i, mol in enumerate(pbar):
-            fps = AllChem.GetMorganFingerprintAsBitVect(mol, radius=radius, nBits=n_bits, useChirality=chirality, useFeatures=features)
-            desc_mat[i] = fps
+            desc_mat = np.zeros((len(all_dataset[0].X), n_bits))
 
-        # create cache dir if not existing
-        Path(PurePosixPath(cached_descriptors)).parent.mkdir(parents=True, exist_ok=True)
+            pbar = tqdm(all_dataset[0].X)
+            for i, mol in enumerate(pbar):
+                fps = AllChem.GetMorganFingerprintAsBitVect(mol, radius=radius, nBits=n_bits, useChirality=chirality, useFeatures=features)
+                desc_mat[i] = fps
 
-        sparse_desc_mat = csr_matrix(desc_mat)
-        save_npz(cached_descriptors, sparse_desc_mat)
+            # create cache dir if not existing
+            Path(PurePosixPath(cached_descriptors)).parent.mkdir(parents=True, exist_ok=True)
 
+            sparse_desc_mat = csr_matrix(desc_mat)
+            save_npz(cached_descriptors, sparse_desc_mat)
+        elif self.featurizer_name == "ecfc":
+            radius = self.kwargs["radius"]
+            chirality = self.kwargs["chirality"] if "chirality" in self.kwargs else False
+            features = self.kwargs["features"] if "features" in self.kwargs else False
 
+            tasks, all_dataset, transformers = dataset_loading_functions[self.name](featurizer="Raw", splitter=None,
+                                                                                    data_dir=self.cache_dir,
+                                                                                    reload=self.use_cache)
+
+            cached_descriptors = self.cache_dir + self.name + "_ecfc" + f"_radius{str(radius)}" + f"_seed{str(self.split_seed)}" + f"_chirality{str(chirality)}" + f"_features{str(features)}" + ".npz"
+            if Path(PurePosixPath(cached_descriptors)).exists() and self.use_cache:
+                return
+
+            X = all_dataset[0].ids
+            y = all_dataset[0].y
+
+            (X_train, y_train), *_ = self.split(X, y)
+            self.featurizer = ECFC_featurizer(radius=radius, useChirality=chirality, useFeatures=features)
+            self.featurizer.fit(X_train)
+
+            desc_mat = self.featurizer.transform(X)
+
+            Path(PurePosixPath(cached_descriptors)).parent.mkdir(parents=True, exist_ok=True)
+            sparse_desc_mat = csr_matrix(desc_mat)
+            save_npz(cached_descriptors, sparse_desc_mat)
+        else:
+            raise ValueError(f"unknown featurizer {self.featurizer_name}")
 
     def setup(self, stage: Optional[str] = None):
         n_bits = self.kwargs["n_bits"]
@@ -144,7 +176,13 @@ class MolNetClassifierDataModule(pl.LightningDataModule):
                                                                                 reload=self.use_cache)
 
         # load descriptors
-        cached_descriptors = self.cache_dir + self.name + "_ecfp" + f"_radius{str(radius)}" + f"_n_bits{str(n_bits)}" + f"_chirality{str(chirality)}" + f"_features{str(features)}" + ".npz"
+        if self.featurizer_name == "ecfp":
+            cached_descriptors = self.cache_dir + self.name + "_ecfp" + f"_radius{str(radius)}" + f"_n_bits{str(n_bits)}" + f"_chirality{str(chirality)}" + f"_features{str(features)}" + ".npz"
+        elif self.featurizer_name == "ecfc":
+            cached_descriptors = self.cache_dir + self.name + "_ecfc" + f"_radius{str(radius)}" + f"_seed{str(self.split_seed)}" + f"_chirality{str(chirality)}" + f"_features{str(features)}" + ".npz"
+        else:
+            raise ValueError(f"unknown featurizer {self.featurizer_name}")
+
         sparse_desc_mat = load_npz(cached_descriptors)
 
         X = sparse_desc_mat.toarray()
@@ -162,30 +200,45 @@ class MolNetClassifierDataModule(pl.LightningDataModule):
                                       seed=self.split_seed)
 
         self.input_size = X.shape[-1]
+        self.categorical_sizes = np.max(X, axis=0) + 1
 
-        # get classes/weights without sorting
-        indices = np.unique(y, return_index=True)[1]
-        self.classes = np.concatenate([y[i] for i in sorted(indices)])
-        self.class_weights = np.concatenate([all_dataset[0].w[i] for i in sorted(indices)]) if hasattr(all_dataset[0], "w") else None
-
-        # sort classes/weights
-        sorted_indices = np.argsort(self.classes)
-        self.classes = self.classes[sorted_indices]
-        self.class_weights = self.class_weights[sorted_indices].tolist() if self.class_weights is not None else None
+        w = all_dataset[0].w if hasattr(all_dataset[0], "w") else None
+        self.classes, self.class_weights = self.determine_classes(y, w)
 
         # default split and create dataset
+        (X_train, y_train), (X_val, y_val), (X_test, y_test) = self.split(X, y)
 
-        test_size = sum(self.split_size[1:])
-        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size, random_state=self.split_seed,
-                                                          stratify=self.classes if self.split == "stratified" else None)
-
-        test_size = self.split_size[-1] / test_size
-        X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=test_size, random_state=self.split_seed,
-                                                        stratify=self.classes if self.split == "stratified" else None)
+        X_train = X_train.astype(np.float32)
+        X_val = X_val.astype(np.float32)
+        X_test = X_test.astype(np.float32)
 
         self.train_dataset = TensorDataset(torch.from_numpy(X_train).float(), torch.from_numpy(y_train).long().squeeze())
         self.val_dataset = TensorDataset(torch.from_numpy(X_val).float(), torch.from_numpy(y_val).long().squeeze())
         self.test_dataset = TensorDataset(torch.from_numpy(X_test).float(), torch.from_numpy(y_test).long().squeeze())
+
+    def determine_classes(self, y, w):
+        # get classes/weights without sorting
+        indices = np.unique(y, return_index=True)[1]
+        classes = np.concatenate([y[i] for i in sorted(indices)])
+        class_weights = np.concatenate([w[i] for i in sorted(indices)]) if w is not None else None
+
+        # sort classes/weights
+        sorted_indices = np.argsort(classes)
+        classes = classes[sorted_indices]
+        class_weights = class_weights[sorted_indices].tolist() if class_weights is not None else None
+
+        return classes, class_weights
+
+    def split(self, X, y):
+        test_size = sum(self.split_size[1:])
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size, random_state=self.split_seed,
+                                                          stratify=self.classes if self.split_type == "stratified" else None)
+
+        test_size = self.split_size[-1] / test_size
+        X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=test_size, random_state=self.split_seed,
+                                                        stratify=self.classes if self.split_type == "stratified" else None)
+
+        return (X_train, y_train), (X_val, y_val), (X_test, y_test)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, pin_memory=True)
@@ -198,7 +251,6 @@ class MolNetClassifierDataModule(pl.LightningDataModule):
             return DataLoader(self.test_dataset, batch_size=len(self.test_dataset), num_workers=self.num_workers, pin_memory=True)
         else:
             return None
-
 
     @rank_zero_only
     def log_hyperparameters(self, logger: LightningLoggerBase, ignore_param: List[str] = None, types: List = None):
