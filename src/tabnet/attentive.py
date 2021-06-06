285bc7c99b0da438e6af06d6248cd72bd18440bf
clemens33 <yongwu33@gmail.com>
2021-06-06 23:48:30 +0200

    - added support for binary mask

--- src/tabnet/attentive.py
+++ src/tabnet/attentive.py
@@ -5,14 +5,14 @@ import torch.nn as nn
 from entmax import Entmax15, Sparsemax
 
 from tabnet.sparsemax import EntmaxBisect, Sparsemax as MySparsemax
-from tabnet.utils import GhostBatchNorm1d
+from tabnet.utils import GhostBatchNorm1d, Round1 as Round, HardSigm2 as HardSigm
 
 
 class Attentive(nn.Module):
-    def __init__(self, dim: int = -1, attentive_type: str = "sparsemax", alpha: Union[nn.Parameter, float] = 2.0):
+    def __init__(self, dim: int = -1, attentive_type: str = "sparsemax", alpha: Union[nn.Parameter, float] = 2.0, **kwargs):
         super(Attentive, self).__init__()
 
-        assert alpha >= 1.0, f"alpha {alpha} must be greater or equal than 1.0"
+        assert alpha >= 1.0 or alpha == -2.0, f"alpha {alpha} must be greater or equal than 1.0"
 
         self.attentive_type = attentive_type
 
@@ -34,6 +34,8 @@ class Attentive(nn.Module):
                 self.attentive = Entmax15(dim=dim)
             elif self.attentive_type == "entmax_bisect":
                 self.attentive = EntmaxBisect(alpha=self.alpha, dim=dim)
+            elif self.attentive_type == "binary_mask":
+                self.attentive = _BinaryMask(**kwargs)
             else:
                 raise ValueError(f"attentive type {attentive_type} is unknown")
 
@@ -53,21 +55,46 @@ class AttentiveTransformer(nn.Module):
                  **kwargs):
         super(AttentiveTransformer, self).__init__()
 
+        assert relaxation_type not in [
+            "gamma_shared_trainable, gamma_trainable, gamma_fixed"], f"relaxation type {relaxation_type} is unknown"
+
         gamma_trainable = (relaxation_type == "gamma_trainable")
         self.gamma = nn.Parameter(torch.scalar_tensor(gamma), requires_grad=gamma_trainable) if isinstance(gamma, float) else gamma
 
         self.fc = nn.Linear(in_features=attentive_size, out_features=input_size, bias=bias)
         self.bn = GhostBatchNorm1d(input_size=input_size, **kwargs)
 
-        self.attentive = Attentive(dim=-1, alpha=alpha, attentive_type=attentive_type)
+        self.attentive = Attentive(dim=-1, alpha=alpha, attentive_type=attentive_type, **kwargs)
 
     def forward(self, feature: torch.Tensor, prior: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        input = self.bn(self.fc(feature))
+        attentive_feature = self.bn(self.fc(feature))
 
-        input = input * prior
-        mask = self.attentive(input)
+        attentive_feature = attentive_feature * prior
+        mask = self.attentive(attentive_feature)
 
         # TODO check what if gamma is an integer
         prior = prior * (self.gamma - mask)
 
         return mask, prior
+
+
+class _BinaryMask(nn.Module):
+    def __init__(self, slope: float = 1.0, slope_type: str = "slope_fixed", th: float = 0.5, **kwargs):
+        super(_BinaryMask, self).__init__()
+
+        self.round = Round(th)
+
+        if slope_type == "slope_hard":
+            self.activation = torch.nn.Hardsigmoid()
+            self.activation.slope = slope
+        else:
+            self.activation = HardSigm(slope=slope, slope_type=slope_type)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        activated = self.activation(inputs)
+        #mask_tilde = torch.sigmoid(inputs)
+        mask = self.round(activated)
+
+        # mask = torch.sigmoid(sz)
+
+        return mask
--- src/tabnet/utils.py
+++ src/tabnet/utils.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch
 import torch.nn as nn
 
@@ -42,3 +44,108 @@ class GhostBatchNorm1d(nn.Module):
         output = output.transpose(-1, 1) if input.ndim == 3 else output
 
         return output
+
+
+class _Round(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x: torch.Tensor, th: float) -> torch.Tensor: # noqa
+        # x[x >= th] = 1
+        # x[x < th] = 0
+
+        # x = torch.where(
+        #     x >= th,
+        #     torch.scalar_tensor(1, device=x.device),
+        #     torch.scalar_tensor(0, device=x.device)
+        # )
+
+        # fastest version
+        x = (x > th).float()
+
+        return x
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor): # noqa
+        dth = None  # indeterminable
+        dx = grad_output  # identity/pass through gradient
+
+        return dx, dth
+
+
+class Round1(nn.Module):
+    def __init__(self, th: float = 0.5):
+        super(Round1, self).__init__()
+
+        self.th = nn.Parameter(torch.scalar_tensor(th), requires_grad=False)
+        self.round = _Round.apply
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.round(x, self.th)
+
+
+
+
+class HardSigm1(nn.Module):
+    def __init__(self, slope: float = 1.0, slope_type: str = "slope_fixed"):
+        super(HardSigm1, self).__init__()
+
+        slope_trainable = (slope_type == "slope_trainable")
+        self.slope = nn.Parameter(torch.scalar_tensor(slope), requires_grad=slope_trainable)
+
+        self.one = nn.Parameter(torch.scalar_tensor(1), requires_grad=False)
+        self.zero = nn.Parameter(torch.scalar_tensor(0), requires_grad=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.max(torch.min((self.slope * x + 1) / 2, self.one), self.zero)
+
+
+class HardSigm2(nn.Module):
+    """fastest version"""
+
+    def __init__(self, slope: float = 1.0, slope_type: str = "slope_fixed"):
+        super(HardSigm2, self).__init__()
+
+        slope_trainable = (slope_type == "slope_trainable")
+        self.slope = nn.Parameter(torch.scalar_tensor(slope), requires_grad=slope_trainable)
+
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        t = 0.5 * (self.slope * x + 1)
+
+        return torch.clamp(t, min=0, max=1)
+
+
+class SlopeScheduler():
+    _name = "hardsigm.slope"
+
+    def __init__(self, params, factor: int = 25, max: int = 5) -> None:
+        super(SlopeScheduler, self).__init__()
+
+        self.params = params
+
+        # a = min(5, a + factor * epoch)
+        self.update = 1 / factor
+        self.max = max
+
+    def step(self) -> None:
+        for name, p in self.params.items():
+            # if not p.requires_grad and self._name in name:
+            if self._name in name:
+                v = p.data + self.update
+                p.data = v if v <= self.max else torch.scalar_tensor(self.max)
+
+    def set_slopes(self, slope: float):
+        for name, p in self.params.items():
+            if self._name in name:
+                p = torch.scalar_tensor(slope)
+
+    def get_slopes(self) -> List[float]:
+        slopes = []
+        for name, p in self.params.items():
+            if self._name in name:
+                slopes.append(float(p.data))
+
+        return slopes
+
+    def get_slope(self, idx: int = 0) -> float:
+        return self.get_slopes()[idx]
