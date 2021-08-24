d24f92250e6c9a72af65fb6024f4da8e1002b1de
clemens33 <yongwu33@gmail.com>
2021-08-24 11:10:23 +0200

    - custom metrics which supports ignore index for accuracy and auroc - tests for custom metrics

--- src/tabnet_lightning/metrics.py
+++ src/tabnet_lightning/metrics.py
@@ -1,5 +1,8 @@
+from typing import List
+
 import torch
 from torchmetrics import Metric
+from torchmetrics.functional import auroc
 
 
 def sparsity(input: torch.Tensor) -> float:
@@ -25,59 +28,121 @@ class Sparsity(Metric):
 
     def compute(self):
         return self.zeros.float() / self.total
-#
-#
-# if __name__ == "__main__":
-#     inputs = torch.LongTensor([
-#         [1, 1, 1, 0],
-#         [0, 0, 0, 0],
-#         [1, 1, 0, 0],
-#     ])
-#
-#     print(sparsity(inputs))
-#
-#     metric = Sparsity()
-#
-#     metric.update(inputs)
-#     metric.update(inputs)
-#     metric.update(inputs)
-#
-#     s = metric.compute()
-#
-#     print(s)
-#
-#     inputs = torch.Tensor([
-#         [0.1, 0.1, 0.1, .0],
-#         [.0, .0, .0, .0],
-#     ])
-#
-#     metric = Sparsity()
-#
-#     metric.update(inputs)
-#     metric.update(inputs)
-#     metric.update(inputs)
-#
-#     s = metric.compute()
-#
-#     print(s)
-#
-#     metric = Sparsity()
-#
-#     inputs = torch.LongTensor([
-#         [1, 1, 1, 0],
-#         [0, 0, 0, 0],
-#         [1, 1, 0, 0],
-#     ])
-#
-#     print(metric(inputs))
-#
-#     inputs = torch.Tensor([
-#         [0.1, 0.1, 0.1, .0],
-#         [.0, .0, .0, .0],
-#     ])
-#
-#     print(metric(inputs))
-#
-#     print(metric.compute())
-#
-#
+
+
+class CustomAccuracy(Metric):
+    """custom binary accuracy implementation - supports multi target binary cases with defined ignore index"""
+
+    def __init__(self, num_targets: int = 1, ignore_index: int = -100, threshold: float = .5, return_verbose: bool = False, dist_sync_on_step=False):
+        super().__init__(dist_sync_on_step=dist_sync_on_step, compute_on_step=True)
+
+        self.ignore_index = ignore_index
+        self.num_targets = num_targets
+        self.threshold = threshold
+        self.return_verbose = return_verbose
+
+        self.add_state("correct", default=torch.tensor([0] * num_targets), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor([0] * num_targets), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, targets: torch.Tensor):
+        assert targets.shape[-1] == self.num_targets if targets.ndim > 1 else True
+        assert preds.shape == targets.shape
+
+        if targets.ndim == 1:
+            targets = targets.unsqueeze(dim=1)
+            preds = preds.unsqueeze(dim=1)
+
+        # assume probabilities
+        if preds.ndim == targets.ndim and preds.is_floating_point():
+            preds = (preds >= self.threshold).long()
+
+        for t in range(self.num_targets):
+            mask = (targets[:, t] != self.ignore_index)
+
+            _preds = preds[mask, t]
+            _targets = targets[mask, t]
+
+            self.correct[t] += torch.sum(torch.tensor(_preds == _targets))
+            self.total[t] += targets[mask, t].numel()
+
+    def compute(self):
+        accuracies = torch.zeros(self.num_targets)
+
+        for i in range(self.num_targets):
+            accuracies[i] = self.correct[i].float() / self.total[i] if self.total[i] > 0 else -1
+
+        accuracies = accuracies[(accuracies >= 0)]
+
+        if self.return_verbose:
+            return accuracies.mean(), accuracies
+        else:
+            return accuracies.mean()
+
+class CustomAUROC(Metric):
+    """custom AUROC implementation - supports multi target binary cases with definable ignore index"""
+
+    def __init__(self, num_targets: int = 1, ignore_index: int = -100, return_verbose: bool = False, dist_sync_on_step=False):
+        """
+
+        Args:
+            num_targets (): expected number of targets
+            ignore_index (): index within targets/labels which should be ignored
+            return_verbose (): whether to return individual aurocs per target or only mean of aurocs (default)
+            dist_sync_on_step ():
+        """
+        super().__init__(dist_sync_on_step=dist_sync_on_step, compute_on_step=True)
+
+        self.ignore_index = ignore_index
+        self.num_targets = num_targets
+        self.return_verbose = return_verbose
+
+        self.add_state("preds", default=[], dist_reduce_fx=CustomAUROC.reduce_fx)
+        self.add_state("targets", default=[], dist_reduce_fx=CustomAUROC.reduce_fx)
+
+    def update(self, preds: torch.Tensor, targets: torch.Tensor):
+        assert targets.shape[-1] == self.num_targets if targets.ndim > 1 else True
+        assert preds.shape == targets.shape
+        assert preds.is_floating_point()
+
+        if targets.ndim == 1:
+            targets = targets.unsqueeze(dim=1)
+            preds = preds.unsqueeze(dim=1)
+
+        _preds, _targets = [], []
+        for t in range(self.num_targets):
+            # add only those predictions which we are not supposed to ignore
+            mask = (targets[:, t] != self.ignore_index)
+
+            _preds.append(preds[mask, t])
+            _targets.append(targets[mask, t])
+
+        self.preds.append(_preds)
+        self.targets.append(_targets)
+
+    def compute(self):
+        aurocs = torch.zeros(self.num_targets)
+        for t in range(self.num_targets):
+            preds, targets = [], []
+            for _preds, _targets in zip(self.preds, self.targets):
+                if _preds[t].numel() > 0 and _targets[t].numel() > 0:
+                    preds.append(_preds[t])
+                    targets.append(_targets[t])
+
+            try:
+                preds = torch.cat(preds)
+                targets = torch.cat(targets)
+
+                aurocs[t] = auroc(preds, targets.long())
+            except Exception as e:
+                aurocs[t] = float("nan")
+
+                #print(e)
+
+        if self.return_verbose:
+            return aurocs[~aurocs.isnan()].mean(), aurocs
+        else:
+            return aurocs[~aurocs.isnan()].mean()
+
+    @staticmethod
+    def reduce_fx(state: List[List[List[torch.Tensor]]]):
+        raise NotImplementedError("reduce fx not yet implemented")
---
+++ tests/tabnet_lightning/test_metrics.py
@@ -0,0 +1,215 @@
+import pytest
+import torch
+
+
+class TestCustomAccuracy():
+    @pytest.mark.parametrize("preds, targets, ignore_index, expected_value", [
+        (torch.Tensor([0.1, 0.5, 0.6, 0.7, 0]), torch.Tensor([0, 1, 0, 0, -100]), -100, .5),
+
+        (torch.Tensor([
+            [0.1, 0.5, 0.6, 0.7],
+        ]), torch.Tensor([
+            [0, 1, 0, 0],
+        ]), -100, .5),
+
+        (torch.Tensor([
+            [1, 1, 1],
+            [1, 1, 0],
+            [1, 1, 0],
+            [1, 1, 0],
+        ]), torch.Tensor([
+            [0, 1, -100],
+            [0, 1, -100],
+            [0, 1, -100],
+            [0, 1, -100],
+        ]), -100, 0.5),
+
+        (torch.Tensor([
+            [1, 1, 1],
+            [1, 1, 0],
+            [0, 1, 0],
+            [0, 1, 0],
+        ]), torch.Tensor([
+            [0, 1, -100],
+            [0, 1, 0],
+            [0, 1, -100],
+            [0, 1, 1],
+        ]), -100, 0.666666),
+
+        (torch.Tensor([
+            [0.1, 1],
+            [0.2, 1],
+            [0.3, 1],
+            [0.5, 1],
+        ]), torch.Tensor([
+            [0, 1],
+            [0, 1],
+            [0, 1],
+            [1, 1],
+        ]), -100, 1.0),
+
+    ])
+    def test_accuracy(self, preds, targets, ignore_index, expected_value):
+        from tabnet_lightning.metrics import CustomAccuracy
+
+        num_targets = targets.shape[-1] if targets.ndim > 1 else 1
+        fn = CustomAccuracy(num_targets, ignore_index, return_verbose=False)
+
+        fn(preds, targets)
+        value = fn.compute()
+
+        assert torch.allclose(value, torch.tensor(expected_value))
+
+
+class TestCustomAUROC():
+    @pytest.mark.parametrize("preds, targets, ignore_index, chunks", [
+        (torch.Tensor([0.1, 0.9]), torch.Tensor([1, 0]), -100, 1),
+
+        (torch.Tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]), torch.Tensor([1, 0, 1, 0, 0, 0, 0, 0, 0]), -100, 9),
+
+        (torch.Tensor([
+            [0.1, 0.3],
+            [0.2, 0.4],
+            [0.3, 0.6],
+            [0.1, 0.1],
+        ]), torch.Tensor([
+            [-100, 1],
+            [-100, 0],
+            [0, -100],
+            [0, 1],
+        ]), -100, 2),
+
+        (torch.Tensor([
+            [0.1, 0.3],
+            [0.2, 0.4],
+            [0.3, 0.6],
+            [0.1, 0.1],
+        ]), torch.Tensor([
+            [-100, 1],
+            [-100, 0],
+            [-100, -100],
+            [-100, 1],
+        ]), -100, 2),
+
+        (torch.Tensor([
+            [0.1, 0.3],
+            [0.2, 0.4],
+            [0.3, 0.6],
+            [0.1, 0.1],
+            [0.3, 0.6],
+            [0.5, 0.1],
+            [0.3, 0.6],
+            [0.5, 0.1],
+        ]), torch.Tensor([
+            [0, 1],
+            [0, 0],
+            [0, -100],
+            [1, 1],
+            [0, -100],
+            [1, 1],
+            [0, -100],
+            [1, 1],
+        ]), -100, 1),
+
+    ])
+    def test_auroc(self, preds, targets, ignore_index, chunks):
+        from tabnet_lightning.metrics import CustomAUROC
+        from sklearn.metrics import roc_auc_score
+        import math
+
+        num_targets = targets.shape[-1] if targets.ndim > 1 else 1
+        fn = CustomAUROC(num_targets, ignore_index, return_verbose=True)
+
+        for _preds, _targets in zip(torch.chunk(preds, chunks), torch.chunk(targets, chunks)):
+            fn(_preds, _targets)
+
+        auroc_mean, aurocs = fn.compute()
+
+        expected_aurocs = torch.zeros(num_targets)
+
+        if targets.ndim == 1:
+            targets = targets.unsqueeze(dim=1)
+            preds = preds.unsqueeze(dim=1)
+
+        for t in range(num_targets):
+
+            mask = (targets[:, t] != ignore_index)
+
+            _targets = targets[mask, t]
+            _preds = preds[mask, t]
+
+            try:
+                expected_auroc = roc_auc_score(_targets, _preds)
+            except Exception as e:
+                expected_auroc = float("nan")
+
+                print(e)
+
+            expected_aurocs[t] = expected_auroc
+
+            if aurocs[t].isnan():
+                assert math.isnan(expected_auroc)
+            else:
+                assert torch.allclose(torch.tensor(expected_auroc).float(), aurocs[t])
+
+        assert torch.allclose(expected_aurocs[~expected_aurocs.isnan()].mean(), auroc_mean)
+
+
+
+
+# TODO add tests for sparsity metrics
+#
+# if __name__ == "__main__":
+#     inputs = torch.LongTensor([
+#         [1, 1, 1, 0],
+#         [0, 0, 0, 0],
+#         [1, 1, 0, 0],
+#     ])
+#
+#     print(sparsity(inputs))
+#
+#     metric = Sparsity()
+#
+#     metric.update(inputs)
+#     metric.update(inputs)
+#     metric.update(inputs)
+#
+#     s = metric.compute()
+#
+#     print(s)
+#
+#     inputs = torch.Tensor([
+#         [0.1, 0.1, 0.1, .0],
+#         [.0, .0, .0, .0],
+#     ])
+#
+#     metric = Sparsity()
+#
+#     metric.update(inputs)
+#     metric.update(inputs)
+#     metric.update(inputs)
+#
+#     s = metric.compute()
+#
+#     print(s)
+#
+#     metric = Sparsity()
+#
+#     inputs = torch.LongTensor([
+#         [1, 1, 1, 0],
+#         [0, 0, 0, 0],
+#         [1, 1, 0, 0],
+#     ])
+#
+#     print(metric(inputs))
+#
+#     inputs = torch.Tensor([
+#         [0.1, 0.1, 0.1, .0],
+#         [.0, .0, .0, .0],
+#     ])
+#
+#     print(metric(inputs))
+#
+#     print(metric.compute())
+#
+#
